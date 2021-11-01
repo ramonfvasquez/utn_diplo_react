3ae2711dde035b274a4449987b53e1ec7275bcf4
Ramón Vásquez <ramvas1984@gmail.com>
2021-11-01 12:04:50 -0300

    Agregué la unidad 19

--- unidad_19/app.js
+++ unidad_19/app.js
@@ -5,13 +5,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var loginRouter = require('./routes/admin/login');
-var adminObrasRouter = require('./routes/admin/obras');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/admin/login');
+var adminObrasRouter = require('./routes/admin/obras');
 
 var app = express();
+var session = require('express-session');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,12 +22,17 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: "fd34s@!@dfa453f3DF#$D&W",
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: !true }
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin/login', loginRouter);
 
 secured = async (req, res, next) => {
   try {
-    console.log(req.session.user_id);
     if (req.session.user_id) {
       next();
     } else {
